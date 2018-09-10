5c145ebdbf6539712b347ef3764d44248d26ba32
Jason Kuruzovich <jkuruzovich@gmail.com>
2018-09-10 14:44:48 -0400

    Fixed build script.

--- build.sh
+++ build.sh
@@ -6,12 +6,12 @@ cd ./source/packages
 source generate.sh
 cd ../../
 make html
-cd ..
-rm -rf ./docs
-rm -rf ./doctrees
-mv ./html ./docs
-cp ./docs-src/CNAME ./docs/CNAME
+rm -rf ../docs
+rm -rf ../doctrees
+mv ../html ../docs
+cp ../docs-src/CNAME ../docs/CNAME
 git add ./docs/*
 git add ./docs-src/*
 git commit -m "Built documentation"
 git push
+cd ..
